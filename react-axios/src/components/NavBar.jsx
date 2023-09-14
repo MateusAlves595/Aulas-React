76cc0f367ca596d528a65eee614fb3f314cc795e
MATEUS ALVES DA SILVA <22282205@dsn127m1249388.cfp127aluno.local>
2023-09-14 16:45:00 -0300

    primeiro commit

---
+++ react-axios/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <nav className='navbar'>
+        <h2>
+            <Link to={"/"}>Blog</Link>
+        </h2>
+        <ul>
+            <li>
+                <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+                <Link to={"/new"} className="newbtn">Novo Post</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ react-axios/src/routes/Home.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import "./Home.css"
+
+const Home = () => {
+
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async() => {
+        console.log("teste")
+    }
+
+    useEffect(() => {
+
+        getPosts()
+
+    }, [])
+
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
